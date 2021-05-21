357515e47f8cba70212c1a59912fc021887f7f29
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-21 18:20:49 +0530

    semantic ui and basic functional components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            Seasons App
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
