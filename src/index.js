a3c604ccf7cc7e2f222a5d8fc23d57f1681927ff
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-22 21:16:52 +0530

    initiating the state object

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const App = () => {
-    
-//     return (
-//         <div>
-//             Seasons App
-//         </div>
-//     )
-// }
-
 class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {lat: null}
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
