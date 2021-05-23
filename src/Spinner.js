83351c4446b97f86f690228351a642fea563fd3f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-23 19:51:07 +0530

    using default props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Spinner = () => {
+const Spinner = (props) => {
     return (
         <div className="ui active dimmer">
-            <div className="ui big text loader">Loading ...</div>
+            <div className="ui big text loader">{props.message}</div>
         </div>
     );
 }
+
+// Using the defualt prop when we do pass the the prop, 
+// rather than using {props.mesage} || "Loading" , defualtProps can be used
+Spinner.defaultProps = {
+    message: 'Loading'
+};
 
 export default Spinner;
