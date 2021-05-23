76d51504129db73a30d05812a7927f67f58ddc9f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-23 18:33:07 +0530

    refactoring the according to componenet lifecycle

--- src/index.js
+++ src/index.js
@@ -6,38 +6,27 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = { lat: null, errorMessage: '' };
-
+    }
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
-
-    componentDidMount() {
-        console.log('My component was rendereed to the screen')
-    }
-
-    componentDidUpdate() {
-        console.log("My component was updated - it rendered")
-    }
     render() {
-        
-            if(!this.state.errorMessage && this.state.lat) {
-                return <div>Latitude: {this.state.lat}</div>
-            }
-            if(this.state.errorMessage && !this.state.lat) {
-                return <div>Error Message: {this.state.errorMessage}</div>
-            }
-            return (
-                <div>
-                  Loading !!
-                </div> 
-            )
-            
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error Message: {this.state.errorMessage}</div>
+        }
+        return (
+            <div>
+                Loading !!
+            </div>
+        )
+
 
     }
 }
